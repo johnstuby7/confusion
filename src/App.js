034202d5a9903fd46457a2376b2eb51a07d21266
johnstuby7 <johnstuby7@gmail.com>
2021-05-31 10:00:44 -0600

    Added in redux reducer and configurestore, updated maincomponent and app.js to use it.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+const store = ConfigureStore();
+
+// Below is how we make it so that the configurestore is available to all files.
+// wrapping everything in Provider
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-       <div className="App">
-         <Main />
-       </div>
-     </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+         <div className="App">
+           <Main />
+         </div>
+       </BrowserRouter>
+     </Provider>
     );
   }
 }
